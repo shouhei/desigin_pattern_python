d3a725eac895865138f8489f1026587a5135f603
山口将平 <shouhei.yamaguchi.be@gmail.com>
2014-12-28 14:02:00 +0900

    remove file and change access modifier

--- iterator/bookshelfiterator.py
+++ iterator/bookshelfiterator.py
@@ -1,12 +1,12 @@
 class BookShelfIterator(object):
     def __init__(self,bookshelf):
-        self.bookshelf = bookshelf
-        self.index = 0
+        self.__bookshelf = bookshelf
+        self.__index = 0
 
     def has_next(self):
-        return self.index < self.bookshelf.get_length()
+        return self.__index < self.__bookshelf.get_length()
 
     def get_next(self):
-        book = self.bookshelf.get_book_at(self.index)
-        self.index += 1
+        book = self.__bookshelf.get_book_at(self.__index)
+        self.__index += 1
         return book
